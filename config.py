1705a0d25eba2aedb3cdf7bbc67f742d7de14da2
synaxxkhushi <149578930+synaxxkhushi@users.noreply.github.com>
2024-12-25 00:59:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ def is_enabled(value, default):
 API_ID = int(environ.get("API_ID", "24267726"))
 API_HASH = environ.get("API_HASH", "7500ba8248548cc3864bd033668f9a9a")
 BOT_TOKEN = environ.get("BOT_TOKEN", "5603694906:AAHP8J5nO4DcEHtx11di3yleYAJ_rQSXoiA")
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002132398644"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002053069822"))
 ADMINS = int(environ.get("ADMINS", "6231550362"))
 DB_URI = environ.get("DB_URI", "mongodb+srv://EXONTESTMONGO:EXONTESTMONGO@cluster0.bviw7ic.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = environ.get("DB_NAME", "EXONTESTMONGO")
